0b16e04589130e34cce8f9231659a7c130bc442e
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2022-12-07 09:05:35 -0300

    Add files via upload

--- layout/layout_folha.py
+++ layout/layout_folha.py
@@ -36,113 +36,227 @@ config={"displayModeBar": True,
 
 def folha_posts(df):
 
-    col2A, col3A, col4A = st.columns([520, 60, 520])
-    with col2A:
-        st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
-                    ">Indicadores Chaves: <i>"+str(len(df))+" publicações</i></h1>",
-                    unsafe_allow_html=True)
-        fig1, fig2 = metricas(df)
-        st.plotly_chart(fig1, use_container_width=True, config=config)
-        st.plotly_chart(fig2, use_container_width=True, config=config)
-    with col3A:
+    st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                ">Tabela Interativa: <i>" + str((len(df))) + "</i> publicações em análise</h1>",
+                unsafe_allow_html=True)
+
+    selected_rows = folha_tabela(df)
+    st.text("")
+    st.text("")
+
+    if len(selected_rows) == 0:
+
+        col2A, col3A, col4A = st.columns([520, 60, 520])
+        with col2A:
+            st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                        ">Indicadores Chaves: <i>"+str(len(df))+" publicações</i></h1>",
+                        unsafe_allow_html=True)
+            fig1, fig2 = metricas(df)
+            st.plotly_chart(fig1, use_container_width=True, config=config)
+            st.plotly_chart(fig2, use_container_width=True, config=config)
+        with col3A:
+            st.text("")
+        with col4A:
+            st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                        ">Comparação entre os Portais</h1>",
+                        unsafe_allow_html=True)
+
+            figB1 = bar_nomes(df)
+            st.plotly_chart(figB1, use_container_width=True, config=config)
+
+
         st.text("")
-    with col4A:
-        st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
-                    ">Análise por Tipo de Publicação</h1>",
+        st.text("")
+
+        st.markdown("<h2 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                    ">Total de <i>Likes</i> e <i>Comentários</i> por <i>Instagram</i></h2>",
                     unsafe_allow_html=True)
-        figA4 = pie3(df)
-        st.plotly_chart(figA4, use_container_width=True, config=config)  # GRÁFICO DE BARRA HORIZONTAL
+        fig =  plot_point_nome(df)
+        st.plotly_chart(fig, use_container_width=True, config=config)
+        st.text("")
+        st.text("")
 
-    st.text("")
-    st.text("")
+        col2, col3, col4 = st.columns([520, 60, 520])
+        try:
+            figC1, figC2 = map(df)
+            with col2:
+                st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                            ">Comparação entre os Horários</h1>",
+                            unsafe_allow_html=True)
+                fig1 = bar_hora(df)
+                st.plotly_chart(fig1, use_container_width=True, config=config)
+            with col3:
+                st.text("")
+            with col4:
+                st.markdown("<h1 style='font-size:143%; text-align: center; color: #5B51D8;'" +
+                            ">Número de Interações por Turno e Dia da Semana</h1>",
+                            unsafe_allow_html=True)
+                st.plotly_chart(figC2, use_container_width=True, config=config)
+        except:
+            with col2:
+                st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                            ">Comparação entre os Horários</h1>",
+                            unsafe_allow_html=True)
+                fig1 = bar_hora(df)
+                st.plotly_chart(fig1, use_container_width=True, config=config)
+            with col3:
+                st.text("")
+            with col4:
+                st.markdown("<h1 style='font-size:143%; text-align: center; color: #5B51D8;'" +
+                            ">Número de Interações por Turno e Dia da Semana</h1>",
+                            unsafe_allow_html=True)
+                st.warning(
+                    'Os dodos selecionados são insuficientes par gerar o Mapa de calor, por favor adcione mais dados!',
+                    icon="⚠️")
 
+        st.text("")
+        st.text("")
 
-    st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
-                ">Número de Publicações por Nome - análise diária</h1>", unsafe_allow_html=True)
-    fig2 = linha_nome2(df)
-    st.plotly_chart(fig2, use_container_width=True, config=config)
-
-    st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
-                ">Total de Interações por Nome - análise diária</h1>", unsafe_allow_html=True)
-    fig = linha_nome(df)
-    st.plotly_chart(fig, use_container_width=True, config=config)
-
-
-    col2, col3, col4 = st.columns([520, 60, 520])
-    with col2:
-        st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
-                    ">Total de atividades de todos os portais</h1>",
-                    unsafe_allow_html=True)
-        figB2 = linha(df)
+        st.markdown("<h2 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                    ">Total de atividades de todos os portais</h2>", unsafe_allow_html=True)
+        figB2 = linha_nome(df)
         st.plotly_chart(figB2, use_container_width=True, config=config)
-    with col3:
         st.text("")
-    with col4:
-        st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
-                    ">Comparação entre os Portais</h1>",
-                    unsafe_allow_html=True)
-
-        figB1 = bar_nomes(df)
-        st.plotly_chart(figB1, use_container_width=True, config=config)
-
-    st.text("")
-    st.text("")
-
-    col2, col3, col4= st.columns([520, 60, 520])
-    with col2:
-        st.markdown("<h1 style='font-size:140%; text-align: center; color: #5B51D8;'" +
-                    ">Comparação entre os Dias da Semana</h1>",
-                    unsafe_allow_html=True)
-        figB1 = bar(df)
-        st.plotly_chart(figB1, use_container_width=True, config=config)  # GRÁFICO DE BARRA HORIZONTAL
-
-    with col3:
         st.text("")
-    with col4:
-        st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
-                    ">Comparação entre os Horários</h1>",
-                    unsafe_allow_html=True)
-        fig1 = bar_hora(df)
-        st.plotly_chart(fig1, use_container_width=True, config=config)
 
-    st.text("")
-    st.text("")
-
-
-    col2, col3, col4 = st.columns([520, 60, 520])
-    try:
-        figC1, figC2 = map(df)
+        col2, col3, col4 = st.columns([520, 60, 520])
         with col2:
-            st.markdown("<h1 style='font-size:140%; text-align: center; color: #5B51D8;'" +
-                        ">Número de Publicações por Turno e Dia da Semana</h1>",
+            fig4 = plot_wordcoud(df)
+            st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                        ">Análise das palavras mais frequentes</h1>",
                         unsafe_allow_html=True)
-            st.plotly_chart(figC1, use_container_width=True, config=config)  # GRÁFICO DE BARRA HORIZONTAL
+            st.pyplot(fig4, use_container_width=True, config=config)
         with col3:
             st.text("")
         with col4:
-            st.markdown("<h1 style='font-size:143%; text-align: center; color: #5B51D8;'" +
-                        ">Número de Interações por Turno e Dia da Semana</h1>",
+            st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                        ">Análise por Tipo de Publicação</h1>",
                         unsafe_allow_html=True)
-            st.plotly_chart(figC2, use_container_width=True, config=config)
-    except:
+            st.text("")
+            figA4 = pie3(df)
+            st.plotly_chart(figA4, use_container_width=True, config=config)
+
+
+        st.text("")
+        st.text("")
+
+
+
+
+
+    elif len(selected_rows) != 0:
+
+        fig = plot_point_nome2(selected_rows)
+        st.plotly_chart(fig, use_container_width=True, config=config)
+
+        col2A, col3A, col4A = st.columns([520, 60, 520])
+        with col2A:
+            st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                        ">Indicadores Chaves: <i>" + str(len(selected_rows)) + " publicações</i></h1>",
+                        unsafe_allow_html=True)
+            fig1, fig2 = metricas(selected_rows)
+            st.plotly_chart(fig1, use_container_width=True, config=config)
+            st.plotly_chart(fig2, use_container_width=True, config=config)
+
+        with col3A:
+            st.text("")
+        with col4A:
+            st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                        ">Análise por Tipo de Publicação</h1>",
+                        unsafe_allow_html=True)
+            figA4 = pie3(selected_rows)
+            st.plotly_chart(figA4, use_container_width=True, config=config)  # GRÁFICO DE BARRA HORIZONTAL
+
+        st.text("")
+        st.text("")
+
+        st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                    ">Total de atividades de todos os portais</h1>", unsafe_allow_html=True)
+        figB2 = linha(selected_rows)
+        st.plotly_chart(figB2, use_container_width=True, config=config)
+        st.text("")
+        st.text("")
+
+        col2, col3, col4 = st.columns([520, 60, 520])
         with col2:
-            st.markdown("<h1 style='font-size:140%; text-align: center; color: #5B51D8;'" +
-                        ">Número de Publicações por Turno e Dia da Semana</h1>",
+            fig4 = plot_wordcoud(selected_rows)
+            st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                        ">Análise das palavras mais frequentes</h1>",
                         unsafe_allow_html=True)
-            st.warning(
-                'Os dodos selecionados são insuficientes par gerar o Mapa de calor, por favor adcione mais dados!',
-                icon="⚠️")
+            st.pyplot(fig4, use_container_width=True, config=config)
         with col3:
             st.text("")
         with col4:
-            st.markdown("<h1 style='font-size:143%; text-align: center; color: #5B51D8;'" +
-                        ">Número de Interações por Turno e Dia da Semana</h1>",
+            st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                        ">Análise por Tipo de Publicação</h1>",
                         unsafe_allow_html=True)
-            st.warning(
-                'Os dodos selecionados são insuficientes par gerar o Mapa de calor, por favor adcione mais dados!',
-                icon="⚠️")
+            figA4 = pie3(df)
+            st.plotly_chart(figA4, use_container_width=True, config=config)  # GRÁFICO DE BARRA HORIZONTAL
+        st.text("")
+        st.text("")
 
-    st.text("")
+        col2, col3, col4 = st.columns([520, 60, 520])
+        with col2:
+            st.markdown("<h1 style='font-size:140%; text-align: center; color: #5B51D8;'" +
+                        ">Comparação entre os Dias da Semana</h1>",
+                        unsafe_allow_html=True)
+            figB1 = bar(selected_rows)
+            st.plotly_chart(figB1, use_container_width=True, config=config)  # GRÁFICO DE BARRA HORIZONTAL
+
+        with col3:
+            st.text("")
+        with col4:
+            st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                        ">Comparação entre os Horários</h1>",
+                        unsafe_allow_html=True)
+            fig1 = bar_hora(selected_rows)
+            st.plotly_chart(fig1, use_container_width=True, config=config)
+
+        st.text("")
+        st.text("")
+
+        col2, col3, col4 = st.columns([520, 60, 520])
+        try:
+            figC1, figC2 = map(selected_rows)
+            with col2:
+                st.markdown("<h1 style='font-size:140%; text-align: center; color: #5B51D8;'" +
+                            ">Número de Publicações por Turno e Dia da Semana</h1>",
+                            unsafe_allow_html=True)
+                st.plotly_chart(figC1, use_container_width=True, config=config)  # GRÁFICO DE BARRA HORIZONTAL
+            with col3:
+                st.text("")
+            with col4:
+                st.markdown("<h1 style='font-size:143%; text-align: center; color: #5B51D8;'" +
+                            ">Número de Interações por Turno e Dia da Semana</h1>",
+                            unsafe_allow_html=True)
+                st.plotly_chart(figC2, use_container_width=True, config=config)
+        except:
+            with col2:
+                st.markdown("<h1 style='font-size:140%; text-align: center; color: #5B51D8;'" +
+                            ">Número de Publicações por Turno e Dia da Semana</h1>",
+                            unsafe_allow_html=True)
+                st.warning(
+                    'Os dodos selecionados são insuficientes par gerar o Mapa de calor, por favor adcione mais dados!',
+                    icon="⚠️")
+            with col3:
+                st.text("")
+            with col4:
+                st.markdown("<h1 style='font-size:143%; text-align: center; color: #5B51D8;'" +
+                            ">Número de Interações por Turno e Dia da Semana</h1>",
+                            unsafe_allow_html=True)
+                st.warning(
+                    'Os dodos selecionados são insuficientes par gerar o Mapa de calor, por favor adcione mais dados!',
+                    icon="⚠️")
+
+        st.text("")
+
+        st.markdown("<h1 style='font-size:150%; text-align: center; color: #5B51D8;'" +
+                    ">Disperção entre Likes e Comentários</h1>", unsafe_allow_html=True)
+        fig = plot_point(selected_rows)
+        st.plotly_chart(fig, use_container_width=True, config=config)
+        st.text("")
+        st.text("")
+
     return None
 
 
