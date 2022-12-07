5658db83d83003163aff7a64ed4917f75007033d
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2022-12-07 09:05:14 -0300

    Add files via upload

--- outros/variaveis_folha.py
+++ outros/variaveis_folha.py
@@ -18,10 +18,17 @@ path_todos = "data/portais_dados.csv"
 
 # - TOPO E RODAPÉ ------------------------------------------------------------------------
 html_title1="""
-<h1 style="font-size:250%; color:#5B51D8; font-family:sans-serif;
-            text-align:center; ">Data App - Monitor Manual</h1>
-<h2 style="font-size:150%; color:#5B51D8; font-family:sans-serif;
-            text-align:center; ">Publicações dos 10 maiores Instagrans de Notícias</h2>
+<h1 style="font-size:200%; color:#5B51D8; font-family:sans-serif; text-align:center"
+>Data App - Instagram Monitore<br>
+ <h2 style="font-size:150%; color:#5B51D8;font-family:sans-serif; text-align:center; "
+ >Publicações dos maiores Instagrans de Notícias do Brasil</h2><br>
+ <hr style= "display: block;
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
+  margin-left: auto;
+  margin-right: auto;
+  border-style: inset;
+  border-width: 1.5px;"></h1>
 """
 
 html_title2="""
@@ -240,3 +247,30 @@ html_card_header_1AA="""
   </div>
 </div>
 """
+
+
+
+
+
+
+html_card_1="""
+<div class="card" style="border-radius: 10px 10px 10px 10px; background: #F5F5F5; 
+                         border:solid; border-color: black; border-width: 1px 1px 1px 1px;
+                         width: 100%; height: 200px;
+                         padding-top: 5px; padding-right: 20px; padding-bottom: 1px; padding-left: 20px;">
+    <p class="card-title" style="background-color:#F5F5F5; color:#0d0d0d; 
+                                  margin-top: 10px; margin-bottom: 10px; margin-left: 10px; margin-right: 10px;
+                                  font-family:sans-serif; text-align: justify;
+                                  font-size: 90%" > A base de dados deste Data App são as publicações do Instagram
+                                  dos principais portais de notícias do Brasil, sendo possível observar diversas características
+                                  e métricas dessas publicações, como número de likes, comentários, interações, data e horário,
+                                  link, id único e mais. Todas essas características podem ser analisadas em
+                                  Tabelas Dinâmicas e visualizadas em Dashboards Interativos.<br> 
+                                  Além da análise e visualização dos dados é possível explorá-los, este Data App contém
+                                  diversos recursos para configurar e filtrar os dados utilizados, assim como, configurar
+                                  todos os gráficos com seus eixos e formatos. Para se aprofundar nas funcionalidades e 
+                                  tutoriais deste app, clique abaixo. 👇
+</p>
+</div>
+"""
+
